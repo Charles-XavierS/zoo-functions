000cc60313a65782670809c08fffab94b5eff3b9
Charles Xavier <charlesxsaldanha@hotmail.com>
2022-01-27 18:41:04 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animalsListObj = {};
+    species.forEach((specie) => {
+      animalsListObj[specie.name] = specie.residents.length;
+      return animalsListObj;
+    });
+    return animalsListObj;
+  }
+
+  if (!animal.sex) {
+    const searchAnimal = species.find((specie) => specie.name === animal.specie);
+    return searchAnimal.residents.length;
+  }
+
+  const animalsCount = species.find((specie) => specie.name === animal.specie)
+    .residents.filter((searchAnimalSex) => searchAnimalSex.sex === animal.sex);
+  return animalsCount.length;
 }
 
 module.exports = countAnimals;
