a82d22f41d54f272ef9fceaf7b5eea1d6b109d0a
Charles Xavier <charlesxsaldanha@hotmail.com>
2022-01-27 16:46:21 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((searchEmployee) => searchEmployee
+    .managers.find((searchManager) => searchManager === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((employee) => employee.managers.includes(managerId))
+    .map((nameEmployee) => `${nameEmployee.firstName} ${nameEmployee.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
