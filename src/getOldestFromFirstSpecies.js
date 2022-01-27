0e031c654fe7026c5610c5f670a2f62006309d40
Charles Xavier <charlesxsaldanha@hotmail.com>
2022-01-27 20:22:04 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idForEmployee = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animalSpecie = species.find((specie) => specie.id === idForEmployee)
+    .residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(animalSpecie);
 }
 
 module.exports = getOldestFromFirstSpecies;
