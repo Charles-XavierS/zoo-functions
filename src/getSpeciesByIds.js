f3e90ff4be5ba22a5407e035ae122bfbac011bb5
Charles Xavier <charlesxsaldanha@hotmail.com>
2022-01-27 01:26:28 -0300

    Requisitos 2 e 3

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return ids.map((id) => species.find((specie) => specie.id === id));
+  return ids.map((id) => species.find((searchId) => searchId.id === id));
 }
 
 module.exports = getSpeciesByIds;
